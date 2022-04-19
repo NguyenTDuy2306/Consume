c6b0ee192cbd3a9d1b11b7a8e3b800f00bf13e67
AAVN\ntduy <ntduy2306@gmail.com>
2022-04-19 09:50:57 +0700

    first commit

---
+++ src/ConsumerExample1.java
@@ -0,0 +1,31 @@
+
+import java.util.function.Consumer;
+
+public class ConsumerExample1 {
+
+    static void printValue(int val) {
+        System.out.println(val);
+    }
+
+    public static void main(String[] args) {
+        // Create Consumer interface
+        Consumer<String> consumer = new Consumer<String>() {
+            @Override
+            public void accept(String name) {
+                System.out.println("Hello, " + name);
+            }
+        };
+        // Calling Consumer method
+        consumer.accept("gpcoder"); // Hello, gpcoder
+
+        // Create Consumer interface with lambda expression
+        Consumer<String> consumer1 = (name) -> System.out.println("Hello, " + name);
+        // Calling Consumer method
+        consumer1.accept("gpcoder"); // Hello, gpcoder
+
+        // Create Consumer interface with method reference
+        Consumer<Integer> consumer2 = ConsumerExample1::printValue;
+        // Calling Consumer method
+        consumer2.accept(12); // 12
+    }
+}
